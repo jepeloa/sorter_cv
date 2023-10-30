5d4b349bff655a08aa6f3ba561a57b826bd18c51
jepeloa <javier.epeloa@mapplics.com>
2023-10-30 18:04:04 -0300

    add files for starting project

---
+++ cv_match.py
@@ -0,0 +1,117 @@
+# Import libraries
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from nltk.tokenize import word_tokenize
+from numpy.linalg import norm
+from termcolor import colored
+import pandas as pd
+import numpy as np
+import requests
+import PyPDF2
+import re
+import plotly.graph_objects as go
+import nltk
+nltk.download('punkt')
+from googletrans import Translator
+import os
+import streamlit as st
+#import slate3k as slate
+#!pip install slate3k
+
+
+
+path_to_folder = './CV/'
+translator = Translator()     #Funcion para traducir al español
+
+def translate_text(text,src,dest):
+    if text == "" or text is None:
+        pass
+    else:
+        return translator.translate(text, src=src, dest=dest).text
+
+
+
+
+def preprocess_text(text):
+    # Convert the text to lowercase
+    text = text.lower()
+    
+    # Remove punctuation from the text
+    text = re.sub('[^a-z]', ' ', text)
+    
+    # Remove numerical values from the text
+    text = re.sub(r'\d+', '', text)
+    
+    # Remove extra whitespaces
+    text = ' '.join(text.split())
+    
+    return text
+
+def evaluate_candidate(input_CV,input_JD,model):
+    v1 = model.infer_vector(input_CV.split())
+    v2 = model.infer_vector(input_JD.split())
+    similarity = 100*(np.dot(np.array(v1), np.array(v2))) / (norm(np.array(v1)) * norm(np.array(v2)))
+    return round(similarity, 2)
+
+
+
+
+
+
+def process_JD_and_get_matches(jd_en,model):
+    all_files = os.listdir(path_to_folder)
+    df = pd.DataFrame(columns=['Filename', 'MatchValue'])
+    # Filtra solo los archivos PDF
+    pdf_files = [file for file in all_files if file.endswith('.pdf')]
+    j=0 #contador de archivos
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(path_to_folder, pdf_file)
+    
+        with open(pdf_path, 'rb') as f:
+            pdf = PyPDF2.PdfReader(f)
+            resume_en = ''
+            for i in range(len(pdf.pages)):
+                pageObj = pdf.pages[i]
+                text_to_translate=pageObj.extract_text()
+                print("longitud " + str(len(text_to_translate)))
+                if len(text_to_translate)<5000 and len(text_to_translate)>0:
+                    resume_en += translate_text(text_to_translate,'es','en')
+                if len(text_to_translate)>5000:
+                    resume_en += translate_text(text_to_translate[:5000],'es','en')
+
+    #print("longitud" + str(len(resume_en)))
+    #resume_en=translate_text(resume,'es','en')
+    input_CV = preprocess_text(resume_en)
+    input_JD = preprocess_text(jd_en)
+    match=evaluate_candidate(input_CV,input_JD, model)
+    print(match)
+    df.loc[j] = {'Filename': pdf_file, 'MatchValue': match}
+    j=j+1
+    print("="*50)
+    df['MatchValue'] = df['MatchValue'].astype(float)
+    df_sorted = df.sort_values(by='MatchValue', ascending=False)
+    return df_sorted
+
+
+
+
+
+
+
+
+def main():
+    model = Doc2Vec.load('cv_job_maching.model')
+    st.title("CV Sorter")
+    st.write("Insert the job description and get the matching CVs.")
+
+    # Text area for the user to input the job description
+    jd = st.text_area("Job Description", "")
+
+    if st.button("Process"):
+        if jd:
+            df_sorted = process_JD_and_get_matches(jd,model)
+            st.write(df_sorted)
+        else:
+            st.write("Please enter a job description to process.")
+
+if __name__ == "__main__":
+    main()
